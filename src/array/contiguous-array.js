725ddc3815f911046c110cc1c304f13f7dd08718
cassiano <cgrsobral@gmail.com>
2024-11-13 11:59:56 -0300

    alterei master

--- src/array/contiguous-array.js
+++ src/array/contiguous-array.js
@@ -20,8 +20,26 @@
  * Note: The length of the given binary array will not exceed 50,000.
  */
 console.clear()
-
 const findMaxLength = nums => {
-  return
+  const map = new Map(); // Para rastrear somas acumuladas
+  map.set(0, -1); // Soma inicial como 0 no índice -1
+  let maxLength = 0;
+  let sum = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i] === 0 ? -1 : 1; // Converte 0 para -1 e mantém 1 como está
+
+    if (map.has(sum)) {
+      maxLength = Math.max(maxLength, i - map.get(sum));
+    } else {
+      map.set(sum, i);
+    }
+  }
+
+  return maxLength;
 };
-console.log(findMaxLength())
+
+// Exemplos
+console.log(findMaxLength([0, 1])); // ➞ 2
+console.log(findMaxLength([0, 1, 0])); // ➞ 2
+console.log(findMaxLength([0, 1, 1, 0, 1, 1, 0, 0])); // ➞ 6
